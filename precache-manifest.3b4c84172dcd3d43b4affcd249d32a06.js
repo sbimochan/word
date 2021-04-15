b34bd88b7b343986ba2dc272a930e6abeee6c40e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-04-15 12:16:53 +0000

    Deploy sbimochan/boggle to github.com/sbimochan/boggle.git:gh-pages

---
+++ precache-manifest.3b4c84172dcd3d43b4affcd249d32a06.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "727f51a839a340927f73f630af3461f8",
+    "url": "/boggle/index.html"
+  },
+  {
+    "revision": "4a9fbabfd0539f5c81be",
+    "url": "/boggle/static/css/2.a2a264fc.chunk.css"
+  },
+  {
+    "revision": "2d254e5a51c7947b43ef",
+    "url": "/boggle/static/css/main.b98d40a8.chunk.css"
+  },
+  {
+    "revision": "4a9fbabfd0539f5c81be",
+    "url": "/boggle/static/js/2.b337ca97.chunk.js"
+  },
+  {
+    "revision": "744b76f766044157687ca8b61b891873",
+    "url": "/boggle/static/js/2.b337ca97.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2d254e5a51c7947b43ef",
+    "url": "/boggle/static/js/main.cf0ac89d.chunk.js"
+  },
+  {
+    "revision": "0551b66d21434d2fd714",
+    "url": "/boggle/static/js/runtime-main.6db15363.js"
+  }
+]);
